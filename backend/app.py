cfa436e19c7a1d2ce96b4afb030eac7105d01daa
aquarius6666 <aqaurius6666@gmail.com>
2021-03-09 14:33:34 +0700

    add database

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,16 @@
 from flask import Flask, jsonify
-
+from .database.model import db
 
 app = Flask(__name__)
-
+HEROKU = "config_heroku.py"
+LOCAL = "config_local.py"
+app.config.from_pyfile(HEROKU)
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({"message" : "It index"})
+
+@app.route('/database/create', methods=['GET'])
+def create_table():
+    db.drop_all()
+    db.create_all()
+    return jsonify({"message" : "Create all table successfully!"}), 200
---
+++ backend/database/model.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Account(db.Model):
+
+    username = db.Column(db.String(36), primary_key=True)
+    password = db.Column(db.String(126))
+    uid = db.Column(db.String(36), db.ForeignKey('info.uid'))
+
+class Info(db.Model):
+
+    uid = db.Column(db.String(36))
+    name = db.Column(db.String(64, convert_unicode=True))
+
