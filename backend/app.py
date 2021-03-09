095056a204d1bf08bd85128dd42275c9681e5159
aquarius6666 <aqaurius6666@gmail.com>
2021-03-09 14:17:59 +0700

    config

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,6 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', method=['GET'])
+@app.route('/', methods=['GET'])
 def index():
     return jsonify({"message" : "It index"})
