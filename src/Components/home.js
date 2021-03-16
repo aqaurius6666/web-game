a3a27805aac63f17c4fb20fcb146109854f9641f
= <=>
2021-03-16 21:42:02 +0700

    add register

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from 'react'
 export const Home = () => {
     const [data, setData] = useState()
-<<<<<<< HEAD
-=======
-
->>>>>>> 4919c8fe1d2e2b1f2dc1b82166d4d51d0066b07c
     return (
         <div>
             It home
--- src/Components/register.js
+++ src/Components/register.js
@@ -1,33 +1,51 @@
 import React from 'react'
+import authenticationService from '../API/authentication_service'
 
 const Register = () => {
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
     const [rePassword, setRePassword] = useState("");
+    const checkPass = () => {
+        if (password === rePassword)
+            return true;
+    }
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        if (checkPass())
+            authenticationService.register(userName, password);
+
+    }
     return (
         <div className="base-contains">
             <div className="header">Register</div>
             <div className="content">
                 <div className="form">
-                    <div className="form-group">
-                        <label htmlFor="username"> Username</label>
-                        <input type="text" name="username" placeholder="username" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password"> Password</label>
-                        <input type="text" name="password" placeholder="password" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="rePassword"> rePassword</label>
-                        <input type="text" name="rePassword" placeholder="rePassword" />
-                    </div>
+                    <form className="register-form" onSubmit={onSubmitForm}>
+                        <div className="form-group">
+                            <label htmlFor="username"> Username</label>
+                            <input name="username" placeholder="username" onChange={(e) => setUserName(e.target.value)} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password"> Password</label>
+                            <input type="password" name="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="rePassword"> rePassword</label>
+                            <input type="password" name="rePassword" placeholder="rePassword" onChange={(e) => setRePassword(e.target.value)} />
+                        </div>
+                        <div className="footer">
+                            <button type="submit" className="btn">
+                                Register
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
-            <div className="footer">
+            {/* <div className="footer">
                 <button type="button" className="btn">
                     Register
-          </button>
-            </div>
+                </button>
+            </div> */}
         </div>
     );
 }; export default Register
