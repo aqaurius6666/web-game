4919c8fe1d2e2b1f2dc1b82166d4d51d0066b07c
aquarius6666 <aqaurius6666@gmail.com>
2021-03-16 21:00:26 +0700

    check

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import {api_get} from './api'
 export const Home = () => {
     const [data, setData] = useState()
-    useEffect(() => {
-        api_get()
-    }, [])
+
     return (
         <div>
             It home
