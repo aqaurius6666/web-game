17d33f331b294d29a1005b446d880beb48d3a5ea
quangvux2001 <71633849+quangvux2001@users.noreply.github.com>
2021-03-18 14:40:06 +0700

    Update navbar.js

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -3,12 +3,12 @@ import React from 'react'
 const NavBar = () => {
 
     return (
-        <>
+        // <>
         <nav className="NavbarItems">
             <h1 className="navbar-logo"></h1>
             <div className="menu-logo"></div>
 
         </nav>
-        </>
+        // </>
     )
 }; export default NavBar
